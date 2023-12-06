3c0424c5e72b648e4a21788dc5f0ae1d7a55c664
Aboubacar Sidig DIALLO <asd1194diallo@gmail.com>
2023-12-06 19:18:50 +0100

    correction de la fonctionnalité balayage

--- list-clients.js
+++ list-clients.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded', function () {
   const cartesContainer = document.getElementById('cartes-container');
 
@@ -6,40 +5,73 @@ document.addEventListener('DOMContentLoaded', function () {
   const donneesJSON = sessionStorage.getItem('donneesCommandes');
 
   if (donneesJSON === "undefined") {
+    const p = afficherMessageErreur();
+    cartesContainer.appendChild(p);
+  } else {
+    const donneesCommandes = JSON.parse(donneesJSON);
+
+    if (donneesCommandes.length === 0) {
       const p = afficherMessageErreur();
       cartesContainer.appendChild(p);
-  } else {
-      const donneesCommandes = JSON.parse(donneesJSON);
+    } else {
+      // Générez les cartes des clients.
+      donneesCommandes.forEach((commande, index) => {
+        const carte = genererCarteClient(commande, index);
+        cartesContainer.appendChild(carte);
 
-      if (donneesCommandes.length === 0) {
-          const p = afficherMessageErreur();
-          cartesContainer.appendChild(p);
-      } else {
-          // Générez les cartes des clients.
-          donneesCommandes.forEach((commande, index) => {
-              const carte = genererCarteClient(commande, index);
-              cartesContainer.appendChild(carte);
+        carte.addEventListener('click', function () {
+          afficherDetailCommande(commande.products);
+          // Sauvegarde la position de défilement dans sessionStorage
+          sessionStorage.setItem('positionScroll', cartesContainer.scrollTop);
+        });
+      });
 
-              // Ajoutez un gestionnaire d'événements au clic sur chaque carte.
-              carte.addEventListener('click', function () {
-                  afficherDetailCommande(commande.products);
-                  // Sauvegarde la position de défilement dans sessionStorage
-                  sessionStorage.setItem('positionScroll', cartesContainer.scrollTop);
-              });
-
-              carte.querySelector('.client-infos').addEventListener('touchmove', function (event) {
-                balayerVersGauche(carte, event)
-              });
-          });
-      }
+      // Appeler la fonction pour chaque carte générée dynamiquement
+      donneesCommandes.forEach((commande, index) => {
+        const carte = cartesContainer.children[index];
+        initDraggableCard(carte);
+      });
+    }
   }
 
   // Restaure la position de défilement après un rechargement de la page
   const positionScroll = sessionStorage.getItem('positionScroll');
   if (positionScroll !== null) {
-      cartesContainer.scrollTop = parseInt(positionScroll, 10);
+    cartesContainer.scrollTop = parseInt(positionScroll, 10);
   }
 });
+
+// Fonction qui initialise le glisser-déposer pour une carte donnée
+function initDraggableCard(carte) {
+  let isDragging = false;
+  let initialX = 0;
+  let currentX = 0;
+
+  const livraisonBtnContainer = carte.querySelector('.livraison-btn-container');
+  const livraisonBtnWidth = carte.querySelector('.livraison-btn').offsetWidth;
+
+  carte.addEventListener('touchstart', handleDragStart, false);
+  carte.addEventListener('touchmove', handleDragMove, false);
+
+  function handleDragStart(event) {
+    isDragging = true;
+    initialX = event.touches[0].clientX - currentX;
+  }
+
+  function handleDragMove(event) {
+    if (!isDragging) return;
+    currentX = event.touches[0].clientX - initialX;
+
+    // Limite le déplacement à la largeur du bouton "Livré" à partir de la droite
+    currentX = Math.min(0, Math.max(-livraisonBtnWidth, currentX));
+
+    carte.style.transform = `translateX(${currentX}px)`;
+
+    // Affiche progressivement le bouton "Livré" à mesure que la carte est balayée
+    const progress = currentX / livraisonBtnWidth;
+    livraisonBtnContainer.style.transform = `translateX(${progress}px)`;
+  }
+}
 
 function genererCarteClient(commande, index) {
   const carte = document.createElement('div');
@@ -78,12 +110,6 @@ function afficherMessageErreur(){
   return paragraphe;
 }
 
-function balayerVersGauche(carte, event){
-  event.stopPropagation();
-   carte.classList.add('balayage-gauche')
-}
-
-
 function afficherDetailCommande(products) {
   // Enregistrez les données des produits de la commande dans le session storage.
   sessionStorage.setItem('commande-products', JSON.stringify(products));
@@ -97,10 +123,10 @@ function confirmerLivraison(event){
 
   // récuperer 
   const carte = event.target.parentNode.previousElementSibling;
-  const conteneur = carte.parentNode;
+  const conteneur = event.target.parentNode.parentNode;
 
-  conteneur.classList.remove('balayage-gauche');
-  carte.classList.add('balayage-droite');
-
+  carte.style.backgroundColor = "#936639";
+  conteneur.style.transform = 'translateX(0)';
   conteneur.style.pointerEvents = 'none';
 }
+
